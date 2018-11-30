08ac25fe3e94de00b4bfcad5dfb21010acdb4cdb
pablogra <pablo.grana@isep.fr>
2018-11-30 16:13:21 +0100

    ajout des classes

---
+++ Dominations/src/Jeu.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Jeu {
+	
+	ArrayList<Joueur> liste_joueurs = new ArrayList<>();
+	int nb_tour;
+	ArrayList<Tuile> liste_tuiles = new ArrayList<>();
+
+}
---
+++ Dominations/src/Plateau.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Plateau {
+	
+	int longueur;
+	int largeur;
+	ArrayList<Tuile> tuile_joueur = new ArrayList<>();
+
+}
